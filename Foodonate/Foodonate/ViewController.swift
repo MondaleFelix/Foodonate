027bc785cf577576a1faebc1ae363c0c8ea5ac9c
Mondale Felix <mondalefelix@gmail.com>
2020-11-29 18:51:50 -0800

    Set up View without storyboards

---
+++ Foodonate/Foodonate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foodonate
+//
+//  Created by Mondale on 11/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
